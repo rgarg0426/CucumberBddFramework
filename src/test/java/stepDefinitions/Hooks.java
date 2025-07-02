1eae7a651888fd83fcb12f59c5d99cf8752b96fd
raviagarwal <raviagarwal@ravis-air.lan>
2025-07-02 22:53:06 +0530

    Adding into branchX

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,7 +1,45 @@
 package stepDefinitions;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Hooks {
 
+	public static WebDriver driver;
+	public static WebDriverWait wait;
+	@Before
+	public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://www.yatra.com/");
+        wait=new WebDriverWait(driver,Duration.ofSeconds(5));
+        
+    }
+	
+	@After
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+	public static WebDriver getDriver()
+	{
+		return driver;
+	}
+	
+	public static WebDriverWait getWait()
+	{
+		return wait;
+	}
 	
 	
 	
